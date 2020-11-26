efae953fac841932c7d662afeebd1e98711a0ddb
mohdtorabi <mitq8.mt@gmail.com>
2020-11-26 20:31:07 +0000

    database.js was fixed to return a promise and a querya

--- 1_queries/1_user_login.sql
+++ 1_queries/1_user_login.sql
@@ -2,9 +2,3 @@ SELECT id, name, email, password
 FROM users
 WHERE email = 'tristanjacobs@gmail.com';
 
-SELECT properties.*, avg(property_reviews.rating) AS average_rating
-FROM properties
-JOIN property_reviews ON properties.id = property_id
-WHERE city = 'Vancouver'
-GROUP BY properties.id
-ORDER BY cost_per_night;
